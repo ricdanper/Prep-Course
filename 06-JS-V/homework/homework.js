171410939b966c4b3be18795ab0927f0ad9eb3da
ricdanper <ridape@hotmail.com>
2022-05-12 10:23:38 -0300

    Tarea de 06 v2

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -71,7 +71,6 @@ function agregarStringInvertida() {
           Edad: this.edad,
           Domicilio: this.domicilio
         }
-        return objetoNuevo;
       } 
 
     }
@@ -81,7 +80,7 @@ function crearInstanciaPersona(nombre, apellido, edad, dir) {
   //Con esta función vamos a crear una nueva persona a partir de nuestro constructor de persona (creado en el ejercicio anterior)
   //Recibirá los valores "Juan", "Perez", 22, "Saavedra 123" para sus respectivas propiedades
   //Devolver la nueva persona creada
-  var persona1 = new Persona ('Juan', 'Perez', 22, 'Saavedra 123');
+  var persona1 = new Persona (nombre, apellido, edad, dir);
   return persona1;
 }
   
